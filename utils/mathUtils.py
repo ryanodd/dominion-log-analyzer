dd04f313958243549b7bcf6ab4f2257b56487664
Ryan Odd <ryanodd@gmail.com>
2020-07-29 21:34:29 -0400

    clean nCr

--- utils/mathUtils.py
+++ utils/mathUtils.py
@@ -1,5 +1,10 @@
 import math
 
+def factorial(x):
+    if (x == 0):
+        return 1
+    else:
+        return math.gamma(x)
+
 def nCr(n,r):
-    f = math.factorial
-    return f(n) / f(r) / f(n-r)
+    return factorial(n) / factorial(r) / factorial(n-r)
