7548803b0b9f06e3e5ec403d5c0646e1197149a9
Ryan Odd <ryanodd@gmail.com>
2020-08-12 00:56:08 -0400

    shop uses hash for listings

--- game/shop.py
+++ game/shop.py
@@ -9,25 +9,27 @@ class Listing:
 
 class Shop:
     def __init__(self, cards, numPlayers):
-        self.listings = []
+        self.listings = {}
 
         victoryAmount = 12
         if (numPlayers <= 2):
             victoryAmount = 8
 
         # TODO: Eventually, this shouldn't live here (and this shouldn't take in numPlayers)
-        self.listings.append(Listing(getCard('Estate'), victoryAmount))
-        self.listings.append(Listing(getCard('Duchy'), victoryAmount))
-        self.listings.append(Listing(getCard('Province'), victoryAmount))
-        self.listings.append(Listing(getCard('Curse'), 30))
-        self.listings.append(Listing(getCard('Copper'), 60))
-        self.listings.append(Listing(getCard('Silver'), 40))
-        self.listings.append(Listing(getCard('Gold'), 30))
+        # make shop listings hash by name
+        self.listings['Estate'] = Listing(getCard('Estate'), victoryAmount)
+        self.listings['Duchy'] = Listing(getCard('Duchy'), victoryAmount)
+        self.listings['Province'] = Listing(getCard('Province'), victoryAmount)
+        self.listings['Curse'] = Listing(getCard('Curse'), 30)
+        self.listings['Copper'] = Listing(getCard('Copper'), 60)
+        self.listings['Silver'] = Listing(getCard('Silver'), 40)
+        self.listings['Gold'] = Listing(getCard('Gold'), 30)
 
         for i in range(len(cards)):
-            self.listings.append(Listing(cards[i], 10))
+            # TODO: Check for triggers here through trigger/event system e.g. Tournament (merchant will use this system too)
+            self.listings[cards[i].name] = Listing(cards[i], 10)
 
-    def pop(self, index):
+    def pop(self, name):
         if (index < 0 or index >= len(self.listings)):
-            logError("Invalid shop pop choice: %s" % index)
+            logError("Invalid shop pop choice: %s" % name)
         return self.listings[index].card
