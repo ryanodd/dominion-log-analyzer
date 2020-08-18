f8e148d9b21f19d5c768f470bf8d2fe3d6a8a4c3
Ryan Odd <ryanodd@gmail.com>
2020-08-17 21:21:35 -0400

    comments about distinguishing short vs. long term choosers

--- bot/choose/choosers.py
+++ bot/choose/choosers.py
@@ -19,6 +19,16 @@ choosers[ChoiceID.BUY] = chooseBuy # Imported
 # 2. The thing you would do otherwise
 # Opportunity cost compare??
 
+def chooseBanditByHighestMoney(choice, gameState, choosingPlayerIndex):
+  maxCost = 0
+  bestCardIndex = None
+  for cardIndex in range(len(gameState.cardStoreStack[-1])):
+    card = gameState.cardStoreStack[-1][cardIndex]
+    if CardType.TREASURE in card.types and card.cost > maxCost:
+      maxCost = card.cost
+      bestCardIndex = cardIndex
+  return bestCardIndex
+
 # TODO: use cardInfo something like 'isBeneficialToHaveInHandThisTurn' calculatable.
 # For now, this could discard Distant Lands over Estate
 def chooseBureaucratByIsVictory(choice, gameState, choosingPlayerIndex):
@@ -42,14 +52,13 @@ def chooseMineOneByLowestCost(choice, gameState, choosingPlayerIndex):
   bestCardIndex = None
   for handIndex in range(len(player.hand)):
     card = player.hand[handIndex]
-    if CardType.TREASURE in card.types and card.cost > minCost:
+    if CardType.TREASURE in card.types and card.cost < minCost:
       minCost = card.cost
       bestCardIndex = handIndex
   return bestCardIndex
 choosers[ChoiceID.MINE1] = chooseMineOneByLowestCost
 
 def chooseMineTwoByHighestCost(choice, gameState, choosingPlayerIndex):
-
   # Possibly referencing the listing map wrong here?
   for listing in gameState.shop.listings:
     if listing.quantity >= 1 and CardType.TREASURE in listing.card.types and listing.card.cost > maxCost:
@@ -72,27 +81,30 @@ def chooseVassalByBeneficial(choice, gameState, choosingPlayerIndex):
   return getCardInfo(cardInQuestion.name).beneficial
 choosers[ChoiceID.VASSAL] = chooseVassalByBeneficial
 
-    # ARTISAN1
-    # ARTISAN2
-    # BANDIT
-      # BUREAUCRAT
-    # CELLAR
-    # CHAPEL
-    # HARBINGER
-    # LIBRARY
-    # MILITIA - generic discard choice
+  # LT = Long Term
+  # ST = Short Term
+
+    # ARTISAN1 - gain to hand LT + ST value compare
+    # ARTISAN2 - topdeck: ST + 1 (with extra ST next turn)
+      # BANDIT - trash opponent treasure: reasonable hardcode (for now)
+      # BUREAUCRAT - topdeck victory: reasonable hardcode (for now)
+    # CELLAR - compare discarded vs potential draw: ST
+    # CHAPEL - ST + LT
+    # HARBINGER - ST (+1?)
+    # LIBRARY - ST (could be hardcodeed)
+    # MILITIA - generic discard choice: ST
       # MINE1
       # MINE2
       # MONEYLENDER
-    # POACHER - generic discard choice
-    # REMODEL1
-    # REMODEL2 - generic gain choice
-    # SENTRY1  - generic trash choice?
-    # SENTRY2
-    # SENTRY3
-    # THRONEROOM - isBeneficial spectrum?
+    # POACHER - absolute ST
+    # REMODEL1 - trash, with gain cost in mind: LT
+    # REMODEL2 - gain: LT
+    # SENTRY1  - trash: LT + ST (of drawing it)
+    # SENTRY2 - discard: ST + 1 probably
+    # SENTRY3 - order: ST, could probably be hardcoded well
+    # THRONEROOM - isBeneficial spectrum? ST or LT, whetever your cloning
       # VASSAL
-    # WORKSHOP - generic gain choice
+    # WORKSHOP - gain: LT
 #------------
 
 def getChooser(choiceID):
