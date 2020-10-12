9f01e78931b1a6aa887cd8bb337e344077d3b359
Ryan Odd <ryanodd@gmail.com>
2020-10-11 21:25:57 -0400

    cors headers allow both http and https

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from utils.cardSorter import sortCardsByTypeThenCost
 def enable_cors(fn):
     def _enable_cors(*args, **kwargs):
         # set CORS headers
-        response.headers['Access-Control-Allow-Origin'] = 'https://councilroom.herokuapp.com'
+        if request.getHeader('Origin').startswith('https'):
+            response.headers['Access-Control-Allow-Origin'] = 'https://councilroom.herokuapp.com'
+        else:
+            response.headers['Access-Control-Allow-Origin'] = 'http://councilroom.herokuapp.com'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
