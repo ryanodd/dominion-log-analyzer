3003fb2359534e6753ff1a94cbc5145b12a27077
Ryan Odd <ryanodd@gmail.com>
2020-10-30 00:31:03 -0400

    Added to cardNameFilter up to Prosperity

--- alchemist/logParser/cardNameFilter.py
+++ alchemist/logParser/cardNameFilter.py
@@ -26,8 +26,6 @@ nameFilter['Golds'] = 'Gold'
 nameFilter['Province'] = 'Province'
 nameFilter['Provinces'] = 'Province'
 
-
-
 nameFilter['Cellar'] = 'Cellar'
 nameFilter['Cellars'] = 'Cellar'
 
@@ -105,3 +103,283 @@ nameFilter['Witches'] = 'Witch'
 
 nameFilter['Artisan'] = 'Artisan'
 nameFilter['Artisans'] = 'Artisan'
+
+################################
+
+nameFilter['Courtyard'] = 'Courtyard'
+nameFilter['Courtyards'] = 'Courtyard'
+
+nameFilter['Lurker'] = 'Lurker'
+nameFilter['Lurkers'] = 'Lurker'
+
+nameFilter['Pawn'] = 'Pawn'
+nameFilter['Pawns'] = 'Pawn'
+
+nameFilter['Masquerade'] = 'Masquerade'
+nameFilter['Masquerades'] = 'Masquerade'
+
+nameFilter['Shanty Town'] = 'Shanty Town'
+nameFilter['Shanty Towns'] = 'Shanty Town'
+
+nameFilter['Steward'] = 'Steward'
+nameFilter['Stewards'] = 'Steward'
+
+nameFilter['Swindler'] = 'Swindler'
+nameFilter['Swindlers'] = 'Swindler'
+
+nameFilter['Wishing Well'] = 'Wishing Well'
+nameFilter['Wishing Wells'] = 'Wishing Well'
+
+nameFilter['Baron'] = 'Baron'
+nameFilter['Barons'] = 'Baron'
+
+nameFilter['Bridge'] = 'Bridge'
+nameFilter['Bridges'] = 'Bridge'
+
+nameFilter['Conspirator'] = 'Conspirator'
+nameFilter['Conspirators'] = 'Conspirator'
+
+nameFilter['Diplomat'] = 'Diplomat'
+nameFilter['Diplomats'] = 'Diplomat'
+
+nameFilter['Ironworks'] = 'Ironworks'
+
+nameFilter['Mill'] = 'Mill'
+nameFilter['Mills'] = 'Mill'
+
+nameFilter['Mining Village'] = 'Mining Village'
+nameFilter['Mining Villages'] = 'Mining Village'
+
+nameFilter['Secret Passage'] = 'Secret Passage'
+nameFilter['Secret Passages'] = 'Secret Passage'
+
+nameFilter['Courtier'] = 'Courtier'
+nameFilter['Courtiers'] = 'Courtier'
+
+nameFilter['Duke'] = 'Duke'
+nameFilter['Dukes'] = 'Duke'
+
+nameFilter['Minion'] = 'Minion'
+nameFilter['Minions'] = 'Minion'
+
+nameFilter['Patrol'] = 'Patrol'
+nameFilter['Patrols'] = 'Patrol'
+
+nameFilter['Replace'] = 'Replace'
+nameFilter['Replaces'] = 'Replace'
+
+nameFilter['Torturer'] = 'Torturer'
+nameFilter['Torturers'] = 'Torturer'
+
+nameFilter['Trading Post'] = 'Trading Post'
+nameFilter['Trading Posts'] = 'Trading Post'
+
+nameFilter['Upgrade'] = 'Upgrade'
+nameFilter['Upgrades'] = 'Upgrade'
+
+nameFilter['Harem'] = 'Harem'
+nameFilter['Harems'] = 'Harem'
+
+nameFilter['Nobles'] = 'Nobles'
+
+################################
+
+nameFilter['Embargo'] = 'Embargo'
+nameFilter['Embargoes'] = 'Embargo'
+
+nameFilter['Haven'] = 'Haven'
+nameFilter['Havens'] = 'Haven'
+
+nameFilter['Lighthouse'] = 'Lighthouse'
+nameFilter['Lighthouses'] = 'Lighthouse'
+
+nameFilter['Native Village'] = 'Native Village'
+nameFilter['Native Villages'] = 'Native Village'
+
+nameFilter['Pearl Diver'] = 'Pearl Diver'
+nameFilter['Pearl Divers'] = 'Pearl Diver'
+
+nameFilter['Ambassador'] = 'Ambassador'
+nameFilter['Ambassadors'] = 'Ambassador'
+
+nameFilter['Fishing Village'] = 'Fishing Village'
+nameFilter['Fishing Villages'] = 'Fishing Village'
+
+nameFilter['Lookout'] = 'Lookout'
+nameFilter['Lookouts'] = 'Lookout'
+
+nameFilter['Smugglers'] = 'Smugglers'
+
+nameFilter['Warehouse'] = 'Warehouse'
+nameFilter['Warehouses'] = 'Warehouse'
+
+nameFilter['Caravan'] = 'Caravan'
+nameFilter['Caravans'] = 'Caravan'
+
+nameFilter['Cutpurse'] = 'Cutpurse'
+nameFilter['Cutpurses'] = 'Cutpurse'
+
+nameFilter['Island'] = 'Island'
+nameFilter['Islands'] = 'Island'
+
+nameFilter['Navigator'] = 'Navigator'
+nameFilter['Navigators'] = 'Navigator'
+
+nameFilter['Pirate Ship'] = 'Pirate Ship'
+nameFilter['Pirate Ships'] = 'Pirate Ship'
+
+nameFilter['Salvager'] = 'Salvager'
+nameFilter['Salvagers'] = 'Salvager'
+
+nameFilter['Sea Hag'] = 'Sea Hag'
+nameFilter['Sea Hags'] = 'Sea Hag'
+
+nameFilter['Treasure Map'] = 'Treasure Map'
+nameFilter['Treasure Maps'] = 'Treasure Map'
+
+nameFilter['Bazaar'] = 'Bazaar'
+nameFilter['Bazaars'] = 'Bazaar'
+
+nameFilter['Explorer'] = 'Explorer'
+nameFilter['Explorers'] = 'Explorer'
+
+nameFilter['Ghost Ship'] = 'Ghost Ship'
+nameFilter['Ghost Ships'] = 'Ghost Ship'
+
+nameFilter['Merchant Ship'] = 'Merchant Ship'
+nameFilter['Merchant Ships'] = 'Merchant Ship'
+
+nameFilter['Outpost'] = 'Outpost'
+nameFilter['Outposts'] = 'Outpost'
+
+nameFilter['Tactician'] = 'Tactician'
+nameFilter['Tacticians'] = 'Tactician'
+
+nameFilter['Treasury'] = 'Treasury'
+nameFilter['Treasuries'] = 'Treasury'
+
+nameFilter['Wharf'] = 'Wharf'
+nameFilter['Wharves'] = 'Wharf'
+
+################################
+
+nameFilter['Potion'] = 'Potion'
+nameFilter['Potions'] = 'Potion'
+
+nameFilter['Transmute'] = 'Transmute'
+nameFilter['Transmutes'] = 'Transmute'
+
+nameFilter['Vineyard'] = 'Vineyard'
+nameFilter['Vineyards'] = 'Vineyard'
+
+nameFilter['Herbalist'] = 'Herbalist'
+nameFilter['Herbalists'] = 'Herbalist'
+
+nameFilter['Apothecary'] = 'Apothecary'
+nameFilter['Apothecaries'] = 'Apothecary'
+
+nameFilter['Scrying Pool'] = 'Scrying Pool'
+nameFilter['Scrying Pools'] = 'Scrying Pool'
+
+nameFilter['University'] = 'University'
+nameFilter['Universities'] = 'University'
+
+nameFilter['Alchemist'] = 'Alchemist'
+nameFilter['Alchemists'] = 'Alchemist'
+
+nameFilter['Familiar'] = 'Familiar'
+nameFilter['Familiars'] = 'Familiar'
+
+nameFilter['Philosopher\'s Stone'] = 'Philosopher\'s Stone'
+nameFilter['Philosopher\'s Stones'] = 'Philosopher\'s Stone'
+
+nameFilter['Golem'] = 'Golem'
+nameFilter['Golems'] = 'Golem'
+
+nameFilter['Apprentice'] = 'Apprentice'
+nameFilter['Apprentices'] = 'Apprentice'
+
+nameFilter['Possession'] = 'Possession'
+nameFilter['Possessions'] = 'Possession'
+
+################################
+
+nameFilter['Platinum'] = 'Platinum'
+nameFilter['Platina'] = 'Platinum'
+
+nameFilter['Colony'] = 'Colony'
+nameFilter['Colonies'] = 'Colony'
+
+nameFilter['Loan'] = 'Loan'
+nameFilter['Loans'] = 'Loan'
+
+nameFilter['Trade Route'] = 'Trade Route'
+nameFilter['Trade Routes'] = 'Trade Route'
+
+nameFilter['Watchtower'] = 'Watchtower'
+nameFilter['Watchtowers'] = 'Watchtower'
+
+nameFilter['Bishop'] = 'Bishop'
+nameFilter['Bishops'] = 'Bishop'
+
+nameFilter['Monument'] = 'Monument'
+nameFilter['Monuments'] = 'Monument'
+
+nameFilter['Quarry'] = 'Quarry'
+nameFilter['Quarries'] = 'Quarry'
+
+nameFilter['Talisman'] = 'Talisman'
+nameFilter['Talisman'] = 'Talisman'
+
+nameFilter['Worker\'s_Village'] = 'Worker\'s_Village'
+nameFilter['Worker\'s_Villages'] = 'Worker\'s_Village'
+
+nameFilter['City'] = 'City'
+nameFilter['Cities'] = 'City'
+
+nameFilter['Contraband'] = 'Contraband'
+nameFilter['Contrabands'] = 'Contraband'
+
+nameFilter['Counting_House'] = 'Counting_House'
+nameFilter['Counting_Houses'] = 'Counting_House'
+
+nameFilter['Mint'] = 'Mint'
+nameFilter['Mints'] = 'Mint'
+
+nameFilter['Mountebank'] = 'Mountebank'
+nameFilter['Mountebanks'] = 'Mountebank'
+
+nameFilter['Rabble'] = 'Rabble'
+nameFilter['Rabbles'] = 'Rabble'
+
+nameFilter['Royal_Seal'] = 'Royal_Seal'
+nameFilter['Royal_Seals'] = 'Royal_Seal'
+
+nameFilter['Vault'] = 'Vault'
+nameFilter['Vaults'] = 'Vault'
+
+nameFilter['Venture'] = 'Venture'
+nameFilter['Ventures'] = 'Venture'
+
+nameFilter['Goons'] = 'Goons'
+
+nameFilter['Grand_Market'] = 'Grand_Market'
+nameFilter['Grand_Markets'] = 'Grand_Market'
+
+nameFilter['Hoard'] = 'Hoard'
+nameFilter['Hoards'] = 'Hoard'
+
+nameFilter['Bank'] = 'Bank'
+nameFilter['Banks'] = 'Bank'
+
+nameFilter['Expand'] = 'Expand'
+nameFilter['Expands'] = 'Expand'
+
+nameFilter['Forge'] = 'Forge'
+nameFilter['Forges'] = 'Forge'
+
+nameFilter['King\'s_Court'] = 'King\'s_Court'
+nameFilter['King\'s_Courts'] = 'King\'s_Court'
+
+nameFilter['Peddler'] = 'Peddler'
+nameFilter['Peddler'] = 'Peddlers'
