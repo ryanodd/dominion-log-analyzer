0df1de7f0483d679064d89f87b15e131cdefce25
Ryan Odd <ryanodd@gmail.com>
2020-10-30 02:36:03 -0400

    Data for Intrigue

--- alchemist/card.py
+++ alchemist/card.py
@@ -29,6 +29,7 @@ class CardParams:
         self.vp = botValue(0)
 
         self.discards = botValue(0)
+        self.cardCostReduction = botValue(0)
 
         self.doesGain = botValue(False)
         self.doesTrash = botValue(False)
@@ -65,21 +66,26 @@ class Card:
         self.vp = cardParams.vp
 
         self.discards = cardParams.discards
+        self.cardCostReduction = cardParams.cardCostReduction
 
         self.doesGain = cardParams.doesGain
         self.doesTrash = cardParams.doesTrash
+        self.doesSift = cardParams.doesSift # First let's define what sifting is
 
-        # Funny one. Used for bot logic only
+        # Deprecated? Base only
+        # Interesting one. Used for bot logic only
         self.beneficial = cardParams.beneficial
 
-        # Careful with these defaults. Not sure about them. Can have card-for-card overriding or one universal alg hmmmmm
+        # "Computed" values.
+        # Careful with these.
+        # TODO Can we have card-for-card overriding?
         self.cantrip = botValue(False, (self.draws.value > 0 and self.actions.value > 0 ))
         self.extraDraws = botValue(max(0, self.draws.value - 1), self.draws.evaluator, self.draws.message, self.draws.importance)
         self.extraActions = botValue(max(0, self.actions.value - 1), self.actions.evaluator, self.actions.message, self.actions.importance)
         self.terminal = botValue(self.isAction.value and self.actions.value == 0)
         self.stop = botValue(self.draws.value == 0)
 
-# Maps card names to Card objects
+# Maps card names to Card constructors
 cardFns = {}
 
 # Default Values for subjective Cards should be worst-case scenario?
@@ -150,8 +156,8 @@ def create_Cellar(numDiscards=None):
     cardParams.isAction = botValue(True)
     cardParams.cost = botValue(2)
     if numDiscards is None:
-        cardParams.discards = botValue(0, lambda: None, 'Depends on how many discards')
-        cardParams.draws = botValue(0, lambda: None, 'Depends on how many discards')
+        cardParams.discards = botValue(0, lambda: None, 'Dependent on number of discards')
+        cardParams.draws = botValue(0, lambda: None, 'Dependent on number of discards')
     elif numDiscards is not None:
         cardParams.discards = botValue(numDiscards)
         cardParams.draws = botValue(numDiscards)
@@ -195,7 +201,7 @@ def create_Merchant(playsSilver=None):
     cardParams.actions = botValue(1)
 
     if playsSilver is None:
-        cardParams.money = botValue(0, lambda: None, '0 normally, 1 if a silver is played')
+        cardParams.money = botValue(0, lambda: None, '1 if a silver is played')
     elif playsSilver:
         cardParams.money = botValue(1)
     elif not playsSilver:
@@ -227,8 +233,8 @@ def create_Vassal(willHitAction=None):
     cardParams.cost = botValue(3)
     cardParams.money = botValue(2, lambda: None)
     if willHitAction is not None:
-        cardParams.actions = botValue(0, lambda: None, 'Worth 1 if it hits an action card')
-        cardParams.draws = botValue(0, lambda: None, 'Worth 1 if it hits an action card')
+        cardParams.actions = botValue(0, lambda: None, '1 if it hits an action card')
+        cardParams.draws = botValue(0, lambda: None, '1 if it hits an action card')
     elif willHitAction:
         cardParams.actions = botValue(1)
         cardParams.draws = botValue(1)
@@ -282,7 +288,7 @@ def create_Moneylender(doesTrashCopper=None):
     cardParams.cost = botValue(4)
 
     if doesTrashCopper is not None:
-        cardParams.money = botValue(0, lambda: None, 'Worth 3 if you trash a copper')
+        cardParams.money = botValue(0, lambda: None, '3 if you trash a copper')
     elif doesTrashCopper:
         cardParams.money = botValue(3)
     elif not doesTrashCopper:
@@ -311,7 +317,7 @@ def create_Throne_Room(cardNameToCopy=None):
     cardParams.cost = botValue(4)
 
     if cardNameToCopy is None:
-        cardParams.actions = botValue(0, lambda: None, 'Worth 1 if you use it on an action (sort of)')
+        cardParams.actions = botValue(0, lambda: None, '1 if you use it on an action (sort of)')
     elif cardNameToCopy:
         cardParams = cardFns[cardNameToCopy]()
         cardParams.isAction = botValue(True)
@@ -329,7 +335,7 @@ def create_Poacher(numEmptyPiles=None):
     cardParams.money = botValue(1)
 
     if numEmptyPiles is None:
-        cardParams.discards = botValue(0, lambda: None, 'Discards if there are empty supply piles')
+        cardParams.discards = botValue(0, lambda: None, '1+ if there are empty supply piles')
     elif numEmptyPiles is not None:
         cardParams.discards = botValue(numEmptyPiles)
 
@@ -382,7 +388,7 @@ def create_Mine(hasUpgradableTreasure=None):
     cardParams.doesTrash = botValue(True)
 
     if hasUpgradableTreasure is None:
-        cardParams.money = botValue(1, lambda: None, 'Only worth 1 if you have an upgradable treasure', 100)
+        cardParams.money = botValue(1, lambda: None, '0 if you don\'t have an upgradable treasure', 100)
     elif hasUpgradableTreasure:
         cardParams.money = botValue(1)
     elif not hasUpgradableTreasure:
@@ -417,7 +423,7 @@ def create_Library(numDraws=None):
     cardParams.isAction = botValue(True)
     cardParams.cost = botValue(5)
     if numDraws is None:
-        cardParams.draws = botValue(3, lambda: None, 'Draws up to 7')
+        cardParams.draws = botValue(3, lambda: None, 'Dependent on hand size')
     elif numDraws is not None:
         cardParams.draws = botValue(numDraws)
     return cardParams
@@ -438,9 +444,373 @@ def create_Artisan():
     cardParams.isAction = botValue(True)
     cardParams.cost = botValue(6)
     cardParams.doesGain = botValue(True)
-    cardParams.discard = botValue(1) # Is this truly a discard? Thinkin bout Secret Passage
+    cardParams.discard = botValue(1) # Is this truly a discard? Topdeck? But, Thinkin bout Secret Passage
     return cardParams
 cardFns['Artisan'] = create_Artisan
+
+#-----------------Intrigue---------------
+
+def create_Courtyard():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(2)
+    cardParams.draw = botValue(3)
+    cardParams.discard = botValue(1) # Is this truly a discard? Topdeck? But, Thinkin bout Secret Passage
+    return cardParams
+cardFns['Courtyard'] = create_Courtyard
+
+def create_Lurker():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(2)
+    cardParams.action = botValue(2)
+    cardParams.doesGain = botValue(True)
+    return cardParams
+cardFns['Lurker'] = create_Lurker
+# NOTE Not accounting for trashing from supply. doesPileControl?
+
+def create_Pawn(choseCard=None, choseAction=None, choseBuy=None, choseMoney=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(2)
+
+    if choseCard is None and choseAction is None and choseBuy is None and choseMoney is None:
+        cardParams.draws = botValue(1, lambda: None, 'Dependent on player choice')
+        cardParams.actions = botValue(1, lambda: None, 'Dependent on player choice')
+        cardParams.buys = botValue(0, lambda: None, 'Dependent on player choice')
+        cardParams.money = botValue(0, lambda: None, 'Dependent on player choice')
+    if choseCard == True:
+        cardParams.draws = botValue(1)
+    if choseAction == True:
+        cardParams.actions = botValue(1)
+    if choseBuy == True:
+        cardParams.buys = botValue(1)
+    if choseMoney == True:
+        cardParams.money = botValue(1)
+
+    return cardParams
+cardFns['Pawn'] = create_Pawn
+
+def create_Masquerade():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(3)
+    cardParams.doesTrash = botValue(True)
+    return cardParams
+cardFns['Masquerade'] = create_Masquerade
+# NOTE Not accounting for Gain (a bad card)
+
+def create_Shanty_Town(hasNoActionCards=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(3)
+    cardParams.actions = botValue(2)
+    
+    if hasNoActionCards is None:
+        cardParams.draws = botValue(0, lambda: None, '2 if no action cards in hand')
+    elif hasNoActionCards == True:
+        cardParams.draws = botValue(2)
+
+    return cardParams
+cardFns['Shanty Town'] = create_Shanty_Town
+
+def create_Steward(choiceIndex=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(3)
+    cardParams.doesTrash = botValue(True)
+    
+    if choiceIndex is None:
+        cardParams.draws = botValue(2, lambda: None, 'Dependent on player choice')
+        cardParams.money = botValue(0, lambda: None, 'Dependent on player choice')
+    elif choiceIndex == 0:
+        cardParams.draws = botValue(2)
+    elif choiceIndex == 1:
+        cardParams.money = botValue(2)
+
+    return cardParams
+cardFns['Steward'] = create_Steward
+
+def create_Swindler():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isAttack = botValue(True)
+    cardParams.cost = botValue(3)
+    cardParams.money = botValue(2)
+    return cardParams
+cardFns['Swindler'] = create_Swindler
+
+def create_Wishing_Well(guessesCorrectly=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(3)
+    cardParams.actions = botValue(1)
+
+    if guessesCorrectly is None:
+        cardParams.draws = botValue(1, lambda: None, '2 if player guesses correctly')
+    elif guessesCorrectly == True:
+        cardParams.draws = botValue(2)
+    elif guessesCorrectly == False:
+        cardParams.draws = botValue(1)
+
+    return cardParams
+cardFns['Wishing Well'] = create_Wishing_Well
+
+def create_Baron(willDiscardEstate=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.buys = botValue(1)
+
+    if willDiscardEstate is None:
+        cardParams.money = botValue(0, lambda: None, '4 if estate is discarded')
+        cardParams.discard = botValue(0, lambda: None, '1 if estate is discarded')
+    elif willDiscardEstate == True:
+        cardParams.money = botValue(4)
+        cardParams.discards = botValue(1)
+    elif willDiscardEstate == False:
+        cardParams.money = botValue(4)
+    return cardParams
+cardFns['Baron'] = create_Baron
+# NOTE missing gaining the estate (bad)
+
+def create_Bridge():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.buys = botValue(1)
+    cardParams.money = botValue(1)
+    cardParams.cardCostReduction = botValue(1)
+    return cardParams
+cardFns['Bridge'] = create_Bridge
+# NOTE this is optimistic that cardCostReduction will be a thing and not just a simple money
+
+def create_Conspirator(playedThreeActions=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.money = botValue(2)
+
+    if playedThreeActions is None:
+        cardParams.draws = botValue(0, lambda: None, '1 if player has played three actions')
+        cardParams.actions = botValue(0, lambda: None, '1 if player has played three actions')
+    elif playedThreeActions == True:
+        cardParams.draws = botValue(1)
+        cardParams.actions = botValue(1)
+
+    return cardParams
+cardFns['Conspirator'] = create_Conspirator
+
+def create_Diplomat(hasFiveOrLessCardsInHand=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isReaction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.draws = botValue(2)
+
+    if hasFiveOrLessCardsInHand is None:
+        cardParams.actions = botValue(0, lambda: None, '2 if player has five or fewer cards in hand')
+    elif hasFiveOrLessCardsInHand == True:
+        cardParams.actions = botValue(2)
+
+    return cardParams
+cardFns['Diplomat'] = create_Diplomat
+# Incomplete - defense
+
+def create_Ironworks(gainedAction=None, gainedTreasure=None, gainedVictory=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.doesGain = botValue(2)
+
+    if gainedAction is None and gainedTreasure is None and gainedVictory is None:
+        cardParams.action = botValue(0, lambda: None, '1 if action card gained')
+        cardParams.money = botValue(0, lambda: None, '1 if treasure card gained')
+        cardParams.draws = botValue(0, lambda: None, '1 if victory card gained')
+    if gainedAction == True:
+        cardParams.action = botValue(1)
+    if gainedTreasure == True:
+        cardParams.money = botValue(1)
+    if gainedVictory == True:
+        cardParams.draws = botValue(1)
+
+    return cardParams
+cardFns['Ironworks'] = create_Ironworks
+
+def create_Mill(willDiscardTwo=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isVictory = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.draws = botValue(1)
+    cardParams.actions = botValue(1)
+    cardParams.vp = botValue(1)
+
+    if willDiscardTwo is None:
+        cardParams.money = botValue(0, lambda: None, '2 if player dicards two cards')
+    elif willDiscardTwo == True:
+        cardParams.money = botValue(2)
+
+    return cardParams
+cardFns['Mill'] = create_Mill
+
+def create_Mining_Village(willTrash=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.draws = botValue(1)
+    cardParams.actions = botValue(2)
+
+    if willTrash is None:
+        cardParams.money = botValue(0, lambda: None, '2 if player trashes Mining Vilage')
+    elif willTrash == True:
+        cardParams.money = botValue(2)
+
+    return cardParams
+cardFns['Mining Village'] = create_Mining_Village
+
+def create_Secret_Passage():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(4)
+    cardParams.draws = botValue(2)
+    cardParams.actions = botValue(1)
+    return cardParams
+cardFns['Secret Passage'] = create_Secret_Passage
+
+def create_Courtier(choseAction=None, choseBuy=None, choseMoney=None, choseGold=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(5)
+
+    if choseAction is None and choseBuy is None and choseMoney is None and choseGold is None:
+        cardParams.actions = botValue(1, lambda: None, 'Dependent on revealed card')
+        cardParams.buys = botValue(1, lambda: None, 'Dependent on revealed card')
+        cardParams.money = botValue(0, lambda: None, 'Dependent on revealed card')
+        cardParams.doesGain = botValue(False, lambda: None, 'Dependent on revealed card')
+    if choseAction == True:
+        cardParams.actions = botValue(1)
+    if choseBuy == True:
+        cardParams.buys = botValue(1)
+    if choseBuy == True:
+        cardParams.money = botValue(3)
+    if choseGold == True:
+        cardParams.doesGain = botValue(True)
+
+    return cardParams
+cardFns['Courtier'] = create_Courtier
+
+def create_Duke(numDuchies=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isVictory = botValue(True)
+    cardParams.cost = botValue(5)
+    if numDuchies is None:
+        cardParams.vp = botValue(0, lambda: None, 'Dependent on number of Duchies in deck')
+    elif numDuchies is not None:
+        cardParams.vp = botValue(numDuchies)
+    return cardParams
+cardFns['Duke'] = create_Duke
+
+def create_Minion(willDiscard=None, numDiscarded=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isAttack = botValue(True)
+    cardParams.cost = botValue(5)
+    if willDiscard is None:
+        cardParams.money = botValue(0, lambda: None, 'Dependent on player choice')
+        cardParams.draws = botValue(0, lambda: None, 'Dependent on player choice')
+        cardParams.discards = botValue(0, lambda: None, 'Dependent on player choice')
+    if willDiscard == False:
+        cardParams.money = botValue(2)
+    if willDiscard == True:
+        cardParams.draws = botValue(4)
+        if numDiscarded is None:
+            cardParams.discards = botValue(4, lambda: None, 'Dependent on hand size')
+        if willDiscard is not None:
+            cardParams.discards = botValue(numDiscarded)
+    cardParams.cost = botValue(2)
+    return cardParams
+cardFns['Minion'] = create_Minion
+# NOTE this is a 2-phase set of parameters
+
+def create_Patrol(numExtraToDraw=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(5)
+    cardParams.doesSift = botValue(True)
+
+    if numExtraToDraw is None:
+        cardParams.draws = botValue(3, lambda: None, 'Dependent on cards in deck')
+    if numExtraToDraw is not None:
+        cardParams.draws = botValue(numExtraToDraw)
+    return cardParams
+cardFns['Patrol'] = create_Patrol
+
+def create_Replace():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isAttack = botValue(True)
+    cardParams.cost = botValue(5)
+    cardParams.doesTrash = botValue(True)
+    cardParams.doesGain = botValue(True)
+    return cardParams
+cardFns['Replace'] = create_Replace
+# TODO topdeck?
+
+def create_Torturer():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.isAttack = botValue(True)
+    cardParams.cost = botValue(5)
+    cardParams.draws = botValue(3)
+    return cardParams
+cardFns['Torturer'] = create_Torturer
+
+def create_Trading_Post():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(5)
+    cardParams.doesTrash = botValue(True)
+    cardParams.doesGain = botValue(True)
+    return cardParams
+cardFns['Trading Post'] = create_Trading_Post
+
+def create_Upgrade():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(5)
+    cardParams.draws = botValue(1)
+    cardParams.actions = botValue(1)
+    cardParams.doesTrash = botValue(True)
+    cardParams.doesGain = botValue(True)
+    return cardParams
+cardFns['Upgrade'] = create_Upgrade
+
+def create_Harem():
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(6)
+    cardParams.money = botValue(2)
+    cardParams.vp = botValue(2)
+    return cardParams
+cardFns['Harem'] = create_Harem
+
+
+def create_Nobles(choiceIndex=None):
+    cardParams = CardParams()
+    cardParams.isAction = botValue(True)
+    cardParams.cost = botValue(6)
+    cardParams.vp = botValue(2)
+    if choiceIndex is None:
+        cardParams.draws = botValue(3, lambda: None, 'Dependent on player choice')
+        cardParams.actions = botValue(0, lambda: None, 'Dependent on player choice')
+    elif choiceIndex == 0:
+        cardParams.draws = botValue(3)
+    elif choiceIndex == 1:
+        cardParams.actions = botValue(2)
+    return cardParams
+cardFns['Nobles'] = create_Nobles
 
 def getCard(name, paramsList=[]):
     if name not in cardFns:
