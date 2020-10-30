3f85e78dcc8746261869633cc3a4a0dcd228f96e
Ryan Odd <ryanodd@gmail.com>
2020-10-30 02:35:34 -0400

    fix removeItemsFromList bug

--- utils/pythonUtils.py
+++ utils/pythonUtils.py
@@ -2,9 +2,8 @@ def getObjectMembers(object):
   members = [attr for attr in dir(object) if not callable(getattr(object, attr)) and not attr.startswith("__")]
   return members 
 
-def removeItemsFromList(originalList, itemsToRemove):
-  newList = []
-  for e in originalList:
-    if e not in itemsToRemove:
-      newList.append(e)
-    originalList = newList
+# once per occurrence in itemsToRemove
+def removeItemsFromList(listReference, itemsToRemove):
+  for e in itemsToRemove:
+    if e in listReference:
+      listReference.remove(e)
