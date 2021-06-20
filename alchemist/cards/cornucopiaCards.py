32baacb11361c8e47200fd57020eaee6f68c014b
Ryan Odd <ryanodd@gmail.com>
2021-06-20 14:30:40 -0400

    Added alchemist card functions for all cards

---
+++ alchemist/cards/cornucopiaCards.py
@@ -0,0 +1,102 @@
+
+#-----------------Cornucopia---------------
+
+from utils.log import logError 
+from alchemist.alchemistTypes import CardValue
+from alchemist.card import CardParams
+
+# Maps card names to Card constructors
+cardFns = {}
+
+def create_Hamlet():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Hamlet'] = create_Hamlet
+
+def create_Fortune_Teller():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Fortune Teller'] = create_Fortune_Teller
+
+def create_Menagerie():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Menagerie'] = create_Menagerie
+
+def create_Farming_Village():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Farming Village'] = create_Farming_Village
+
+def create_Horse_Traders():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Horse Traders'] = create_Horse_Traders
+
+def create_Remake():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Remake'] = create_Remake
+
+def create_Tournament():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Tournament'] = create_Tournament
+
+def create_Bag_of_Gold():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Bag of Gold'] = create_Bag_of_Gold
+
+def create_Diadem():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Diadem'] = create_Diadem
+
+def create_Followers():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Followers'] = create_Followers
+
+def create_Princess():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Princess'] = create_Princess
+
+def create_Trusty_Steed():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Trusty Steed'] = create_Trusty_Steed
+
+def create_Young_Witch():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Young Witch'] = create_Young_Witch
+
+def create_Harvest():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Harvest'] = create_Harvest
+
+def create_Horn_of_Plenty():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Horn of Plenty'] = create_Horn_of_Plenty
+
+def create_Hunting_Party():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Hunting Party'] = create_Hunting_Party
+
+def create_Jester():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Jester'] = create_Jester
+
+def create_Fairgrounds():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Fairgrounds'] = create_Fairgrounds
+
+def getCornucopiaCardFns():
+  return cardFns
---
+++ alchemist/cards/renaissanceCards.py
@@ -0,0 +1,142 @@
+
+#-----------------Renaissance---------------
+
+from utils.log import logError 
+from alchemist.alchemistTypes import CardValue
+from alchemist.card import CardParams
+
+# Maps card names to Card constructors
+cardFns = {}
+
+def create_Border_Guard():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Border Guard'] = create_Border_Guard
+
+def create_Ducat():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Ducat'] = create_Ducat
+
+def create_Lackeys():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Lackeys'] = create_Lackeys
+
+def create_Acting_Troupe():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Acting Troupe'] = create_Acting_Troupe
+
+def create_Cargo_Ship():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Cargo Ship'] = create_Cargo_Ship
+
+def create_Experiment():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Experiment'] = create_Experiment
+
+def create_Improve():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Improve'] = create_Improve
+
+def create_Flag_Bearer():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Flag Bearer'] = create_Flag_Bearer
+
+def create_Hideout():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Hideout'] = create_Hideout
+
+def create_Inventor():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Inventor'] = create_Inventor
+
+def create_Mountain_Village():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Mountain Village'] = create_Mountain_Village
+
+def create_Patron():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Patron'] = create_Patron
+
+def create_Priest():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Priest'] = create_Priest
+
+def create_Research():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Research'] = create_Research
+
+def create_Research():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Research'] = create_Research
+
+def create_Silk_Merchant():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Silk Merchant'] = create_Silk_Merchant
+
+def create_Old_Witch():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Old Witch'] = create_Old_Witch
+
+def create_Recruiter():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Recruiter'] = create_Recruiter
+
+def create_Scepter():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Scepter'] = create_Scepter
+
+def create_Scholar():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Scholar'] = create_Scholar
+
+def create_Sculptor():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Sculptor'] = create_Sculptor
+
+def create_Seer():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Seer'] = create_Seer
+
+def create_Spices():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Spices'] = create_Spices
+
+def create_Swashbuckler():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Swashbuckler'] = create_Swashbuckler
+
+def create_Treasurer():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Treasurer'] = create_Treasurer
+
+def create_Villain():
+  cardParams = CardParams()
+  return cardParams
+cardFns['Villain'] = create_Villain
+
+def getRenaissanceCardFns():
+  return cardFns
